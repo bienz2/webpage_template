2d706a51b47705d0b08a89ad3a21a5ded0221a68
Amanda Bienz <abienz@Amandas-MacBook-Pro.local>
2021-08-19 09:03:38 -0600

    Added webpage template

---
+++ code.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Amanda Bienz Graduate Student at University of Illinois</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav id="myNavbar" class="navbar navbar-default navbar-inverse navbar-fixed-top" role="navigation">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbarCollapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Amanda Bienz</a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="nav navbar-nav">
+                <li><a href="http://bienz2.web.engr.illinois.edu">Home</a></li>
+                <li><a href="http://bienz2.web.engr.illinois.edu/cv.php">CV</a></li>
+                <li><a href="http://bienz2.web.engr.illinois.edu/contact.php">Contact</a></li>
+                <li><a href="http://bienz2.web.engr.illinois.edu/presentations.php">Presentations</a></li>
+                <li><a href="http://bienz2.web.engr.illinois.edu/publications.php">Papers</a></li>
+                <li class="active"><a href="http://bienz2.web.engr.illinois.edu/code.php">Code</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<div class="container">
+    <div class="jumbotron">
+        <h3>Available Codebases</h3>
+
+        <li><a href="https://github.com/lukeolson/raptor">RAPtor: Parallel Algebraic Multigrid with Reduced Communication</a></li>
+        <li><a href="https://github.com/bienz2/Node_Aware_MPI">Node-Aware MPI Library</a></li>
+
+    </div>
+</div>
+</body>
+</html>                                		
